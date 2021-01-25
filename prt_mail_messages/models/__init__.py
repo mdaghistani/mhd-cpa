065ecaec20374d0ddc19b8cdf4d6d7c6db637c6b
mdaghistani <75797279+mdaghistani@users.noreply.github.com>
2021-01-25 23:35:31 +0300

    Delete prt_mail_messages directory

--- prt_mail_messages/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import prt_mail
-from . import res_partner
-from . import conversation
-from . import res_config_settings
--- prt_mail_messages/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import mail_compose_message
-from . import message_assign_autor
-from . import message_edit
-from . import message_move
